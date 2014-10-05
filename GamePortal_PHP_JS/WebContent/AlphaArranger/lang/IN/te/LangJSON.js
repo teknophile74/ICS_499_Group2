fb49b7d3fb9b6448b8e8a9265660b5dd597fc894
Aaron <burke.na.mn@gmail.com>
2014-10-04 22:21:13 -0500

    Added back telugu JASON array

---
+++ GamePortal_PHP_JS/WebContent/AlphaArranger/lang/IN/te/LangJSON.js
@@ -0,0 +1,211 @@
+/*
+* Telugu
+*/
+
+var Telugu = 
+[
+	{
+		char: "0c05",
+		symbol: "అ"
+	},
+	{
+		char: "0c06",
+		symbol: "ఆ"
+	},
+	{
+		char: "0c07",
+		symbol: "ఇ"
+	},
+	{
+		char: "0c08",
+		symbol: "ఈ"
+	},
+	{
+		char: "0c09",
+		symbol: "ఉ"
+	},
+	{
+		char: "0c0a",
+		symbol: "ఊ"
+	},
+	{
+		char: "0c0b",
+		symbol: "ఋ"
+	},
+	{
+		char: "0c60",
+		symbol: "ౠ"
+	},
+	{
+		char: "0c0e",
+		symbol: "ఎ"
+	},
+	{
+		char: "0c0f",
+		symbol: "ఏ"
+	},
+	{
+		char: "0c10",
+		symbol: "ఐ"
+	},
+	{
+		char: "0c12",
+		symbol: "ఒ"
+	},
+	{
+		char: "0c13",
+		symbol: "ఓ"
+	},
+	{
+		char: "0c14",
+		symbol: "ఔ"
+	},
+	{
+		char: "0c05+0co2",
+		symbol: "అం"
+	},
+	{
+		char: "0c05+0co3",
+		symbol: "అః"
+	},	
+	{
+		char: "0c15",
+		symbol: "క"
+	},
+	{
+		char: "0c16",
+		symbol: "ఖ"
+	},
+	{
+		char: "0c17",
+		symbol: "గ"
+	},
+	{
+		char: "0c18",
+		symbol: "ఘ"
+	},
+	{
+		char: "0c19",
+		symbol: "ఙ"
+	},
+	{
+		char: "0c1a",
+		symbol: "చ"
+	},
+	{
+		char: "0c1b",
+		symbol: "ఛ"
+	},
+	{
+		char: "0c1c",
+		symbol: "జ"
+	},
+	{
+		char: "0c1d",
+		symbol: "ఝ"
+	},
+	{
+		char: "0c1e",
+		symbol: "ఞ"
+	},
+	{
+		char: "0c1f",
+		symbol: "ట"
+	},
+	{
+		char: "0c20",
+		symbol: "ఠ"
+	},
+	{
+		char: "0c21",
+		symbol: "డ"
+	},
+	{
+		char: "0c22",
+		symbol: "ఢ"
+	},
+	{
+		char: "0c23",
+		symbol: "ణ"
+	},
+	{
+		char: "0c24",
+		symbol: "త"
+	},
+	{
+		char: "0c25",
+		symbol: "థ"
+	},
+	{
+		char: "0c26",
+		symbol: "ద"
+	},
+	{
+		char: "0c27",
+		symbol: "ధ"
+	},
+	{
+		char: "0c28",
+		symbol: "న"
+	},
+	{
+		char: "0c2a",
+		symbol: "ప"
+	},
+	{
+		char: "0c2b",
+		symbol: "ఫ"
+	},
+	{
+		char: "0c2c",
+		symbol: "బ"
+	},
+	{
+		char: "0c2d",
+		symbol: "భ"
+	},
+	{
+		char: "0c2e",
+		symbol: "మ"
+	},
+	{
+		char: "0c2f",
+		symbol: "య"
+	},
+	{
+		char: "0c30",
+		symbol: "ర"
+	},
+	{
+		char: "0c32",
+		symbol: "ల"
+	},
+	{
+		char: "0c35",
+		symbol: "వ"
+	},
+	{
+		char: "0c36",
+		symbol: "శ"
+	},
+	{
+		char: "0c37",
+		symbol: "ష"
+	},
+	{
+		char: "0c38",
+		symbol: "స"
+	},
+	{
+		char: "0c39",
+		symbol: "హ"
+	},
+	{
+		char: "0c33",
+		symbol: "ళ"
+	},
+	{
+		char: "0c15",
+		symbol: "క్ష"
+	}
+];
