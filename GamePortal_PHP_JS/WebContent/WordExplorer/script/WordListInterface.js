773e210398659aecc98d51e782f050b43a8928d0
Aaron <burke.na.mn@gmail.com>
2014-10-06 22:42:07 -0500

    Update WordListInterface to remove JS errors

--- GamePortal_PHP_JS/WebContent/WordExplorer/script/WordListInterface.js
+++ GamePortal_PHP_JS/WebContent/WordExplorer/script/WordListInterface.js
@@ -2,17 +2,21 @@
  *  Group 2
  */
 
-function WordListInterface {
+function WordListInterface() {
 	// Interface Methods
 	 var addWord = newWord() //function Word ; //boolean
 	 var removeWord = newWord() //function Word; //boolean
-	 var getWord(wordNum); //Word
-	 var getSize(); //int
+	 this.getWord = function(wordNum) 
+	 {
+		 var wordToGet = wordNum;
+		 // TODO - Get the word
+	 } //Word
+	 var getSize; //int
 	 var searchWordIsDup(wordId); //boolean
 	 var searchWordEnglish(wordInEnglish); //int
 	 var searchWordLang(wordInLang); //int
 	 var searchWordId(wordId); //int
-	 var getEngList(); //List<String>
-	 var isEmpty(); //boolean
-	 var isFull(); //boolean
+	 var getEngList; //List<String>
+	 var isEmpty; //boolean
+	 var isFull; //boolean
 }
