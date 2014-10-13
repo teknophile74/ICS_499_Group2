12c0bf50a7059ca049b51aa33f7ab17c911d2b61
rays3ng <rays3ng@gmail.com>
2014-10-12 21:28:53 -0500

    Adding New files

---
+++ GamePortal_PHP_JS/WebContent/WordExplorer/lang/CurrentLangDirs.js
@@ -0,0 +1,28 @@
+/*
+* Defines the current directories for language arrays defined on the server
+* Written by upload process for new languages
+*/
+
+var CurrentLangDirs = 
+[
+	{
+		dir: "IN_hi_te",
+		PrimLang: "Hindi",
+		AltLang: "Telugu"
+	},
+	{
+		dir: "IN_te_en",
+		PrimLang: "Telugu",
+		AltLang: "English"
+	},
+	{
+		dir: "US_en_te",
+		PrimLang: "English",
+		AltLang: "Telugu"
+	},
+	{
+		dir: "US_en_hi",
+		PrimLang: "English",
+		AltLang: "Hindi"
+	}
+];
