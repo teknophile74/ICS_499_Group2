cafcbafdf0c61b8d802d47e04f05e8cc0bb4e879
Cam <camellia.kalra@gmail.com>
2014-10-24 21:50:01 -0500

    Committed by Cam

---
+++ GamePortal_PHP_JS/WebContent/portal 3/games/AlphaArranger/lang/CurrentLangDirs.js
@@ -0,0 +1,20 @@
+/*
+* Defines the current directories for language arrays defined on the server
+* Written by upload process for new languages
+*/
+
+var CurrentLangDirs = 
+[
+	{
+		dir: "IN_hi",
+		arrayName: "Hindi"
+	},
+	{
+		dir: "IN_te",
+		arrayName: "Telugu"
+	},
+	{
+		dir: "US_en",
+		arrayName: "English"
+	},
+];
